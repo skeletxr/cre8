c32656dafc3406affcd9febf8dbe900d23e35e68
unknown <saketrazdan1@gmail.com>
2025-04-02 22:55:53 +0530

    init and nav

---
+++ starter-template/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Copy your configuration from next.config.ts here
+  reactStrictMode: true,
+  swcMinify: true,
+  // Other config options...
+};
+
+module.exports = nextConfig;
