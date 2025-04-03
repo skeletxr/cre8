912ecada6be249c914603275a669121779ea1d32
saket <saketrazdan1@gmail.com>
2025-04-03 16:28:11 +0530

    introduction without animation

--- starter-template/src/sections/Introduction.tsx
+++ starter-template/src/sections/Introduction.tsx
@@ -1,5 +1,20 @@
-const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+import Tag from "@/components/Tag";
+
+const text = ` Whether it's building your audience, monetizing your work, or scaling your brand—we're here to make it easier.`;
 
 export default function Introduction() {
-    return <div>Introduction</div>;
+    return (
+        <section  className="py-28 lg:40">
+            <div className="container">
+                <div className="flex justify-center">
+                <Tag>Introducing Creatathon</Tag> 
+                </div>
+                <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">                  
+                    <span>"You create, we help you grow." </span>{""}
+                    <span className="text-white/15">{text}</span>
+                    <span className="text-lime-400">That&apos;s why we built Createathon.</span>
+                </div>
+            </div>
+        </section>
+    );
 }
