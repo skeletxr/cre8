95a638c18255995722ac98c6a9d916329b1146b8
saket <146104497+skeletxr@users.noreply.github.com>
2025-04-06 12:27:28 +0530

    Delete starter-template directory

--- starter-template/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Copy your configuration from next.config.ts here
-  reactStrictMode: true,
-  swcMinify: true,
-  // Other config options...
-};
-
-module.exports = nextConfig;
